941e05d22c368f5449d4a3efb079376d260edaec
infamousbazooka <magnusfernandes1295@gmail.com>
2018-06-23 17:06:42 +0530

    Login page design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { map, mergeMap, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) { }
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).pipe(map(() => this.activatedRoute))
+    .pipe(map(route => {
+      while (route.firstChild) route = route.firstChild;
+      return route;
+    }))
+    .pipe(filter(route => route.outlet === 'primary'))
+    .pipe(mergeMap(route => route.data))
+    .subscribe(event => this.titleService.setTitle(event['title']));
+  }
 }
