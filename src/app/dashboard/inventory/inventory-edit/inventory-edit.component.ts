11d1ca6b749d440dc19f3465d9a32bba82f30f6b
infamousbazooka <magnusfernandes1295@gmail.com>
2018-07-26 10:29:50 +0530

    Inventory form.

---
+++ src/app/dashboard/inventory/inventory-edit/inventory-edit.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Store } from "@ngrx/store";
+
+import * as fromRoot from "../../../shared/reducers";
+import * as inventoryActions from "../../../shared/actions/inventory.actions";
+
+@Component({
+  selector: 'app-inventory-edit',
+  templateUrl: './inventory-edit.component.html',
+  styleUrls: ['./inventory-edit.component.scss']
+})
+export class InventoryEditComponent implements OnInit {
+
+  public itemForm: FormGroup;
+  public addItem: boolean;
+  public categories: any[] = [
+    {
+      display: 'Automotive connector',
+      value: 'automotive_connector'
+    },
+    {
+      display: 'Tool',
+      value: 'tool'
+    },
+    {
+      display: 'Raw material',
+      value: 'raw_material'
+    },
+    {
+      display: 'Finished product',
+      value: 'finished_product'
+    },
+    {
+      display: 'Other',
+      value: 'other'
+    }
+  ];
+
+  constructor(
+    private _store: Store<fromRoot.State>,
+    private _activatedRoute: ActivatedRoute,
+    private _fb: FormBuilder,
+    public _location: Location
+  ) {
+    this._activatedRoute.queryParams.subscribe(params => {
+      if (params["id"]) {
+        this.addItem = false;
+        this._store.dispatch(new inventoryActions.FetchInventoryAction(params["id"]));
+      } else {
+        this.addItem = true;
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.itemForm = this._fb.group({
+      id: null,
+      category: [null, Validators.required],
+      description: [null, Validators.required],
+      item_code: [null, Validators.required]
+    });
+  }
+
+  get category(): FormControl {
+    return this.itemForm.get('category') as FormControl;
+  }
+
+  get description(): FormControl {
+    return this.itemForm.get('description') as FormControl;
+  }
+
+  get item_code(): FormControl {
+    return this.itemForm.get('item_code') as FormControl;
+  }
+
+}
--- src/app/dashboard/inventory/inventory.module.ts
+++ src/app/dashboard/inventory/inventory.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgStringPipesModule } from 'ngx-pipes';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { InventoryComponent } from './inventory.component';
 import { InventoryTableComponent } from './inventory-table/inventory-table.component';
+import { InventoryEditComponent } from './inventory-edit/inventory-edit.component';
 
 const routes: Routes = [
+  {
+    path: 'edit',
+    component: InventoryEditComponent,
+    data: {
+      title: "Inventory edit - Dashboard | Gemeni India"
+    }
+  },
   {
     path: '',
     component: InventoryComponent,
@@ -20,10 +30,12 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     NgxPaginationModule,
     NgStringPipesModule,
+    NgSelectModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [InventoryComponent, InventoryTableComponent]
+  declarations: [InventoryComponent, InventoryTableComponent, InventoryEditComponent]
 })
 export class InventoryModule { }
