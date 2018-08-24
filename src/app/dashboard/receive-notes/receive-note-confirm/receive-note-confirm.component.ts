f5c8661e47dcdb96fe120cb352bdc9635dd9eccf
infamousbazooka <magnusfernandes1295@gmail.com>
2018-08-25 02:09:44 +0530

    Imports and indentation

--- src/app/dashboard/receive-notes/receive-note-confirm/receive-note-confirm.component.ts
+++ src/app/dashboard/receive-notes/receive-note-confirm/receive-note-confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material';
 import { Store } from '@ngrx/store';
 
