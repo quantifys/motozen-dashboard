bcf6849e301c463153fee1de17940961cfd631bb
infamousbazooka <magnusfernandes1295@gmail.com>
2018-08-25 02:45:37 +0530

    404 page works.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: 'login'
+  },
+  {
+    path: '**',
+    loadChildren: './error/error.module#ErrorModule'
   }
 ];
 
