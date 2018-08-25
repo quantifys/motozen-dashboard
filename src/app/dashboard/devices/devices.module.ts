adb877e50f3d1e26ddc62441937c71469f664664
infamousbazooka <magnusfernandes1295@gmail.com>
2018-08-26 02:12:38 +0530

    Trasnfer devices component.

--- src/app/dashboard/devices/devices.module.ts
+++ src/app/dashboard/devices/devices.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { DevicesComponent } from './devices.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { MatButtonModule } from '@angular/material';
 import { NgStringPipesModule } from 'ngx-pipes';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -10,27 +12,35 @@ import { DeviceTableComponent } from './device-table/device-table.component';
 import { DeviceDetailComponent } from './device-detail/device-detail.component';
 import { DeviceEditComponent } from './device-edit/device-edit.component';
 import { SldComponent } from './device-edit/sld/sld.component';
+import { DeviceTransferComponent } from './device-transfer/device-transfer.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DevicesComponent,
     data: {
-      title: "Dashboard - Device management | Gemeni India"
+      title: "Device management - Dashboard | Gemeni India"
     }
   },
   {
     path: 'edit',
     component: DeviceEditComponent,
     data: {
-      title: "Dashboard - Device edit | Gemeni India"
+      title: "Device edit - Dashboard | Gemeni India"
+    }
+  },
+  {
+    path: 'transfer',
+    component: DeviceTransferComponent,
+    data: {
+      title: "Device transfer - Dashboard | Gemeni India"
     }
   },
   {
     path: 'view',
     component: DeviceDetailComponent,
     data: {
-      title: "Dashboard - Device details | Gemeni India"
+      title: "Device details - Dashboard | Gemeni India"
     }
   }
 ];
@@ -39,10 +49,12 @@ const routes: Routes = [
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    MatButtonModule,
+    NgSelectModule,
     NgxPaginationModule,
     NgStringPipesModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [DevicesComponent, DeviceTableComponent, DeviceEditComponent, DeviceDetailComponent, SldComponent]
+  declarations: [DevicesComponent, DeviceTableComponent, DeviceEditComponent, DeviceDetailComponent, SldComponent, DeviceTransferComponent]
 })
 export class DevicesModule { }
