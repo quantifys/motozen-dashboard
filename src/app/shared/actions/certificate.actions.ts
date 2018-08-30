7cfd59241ed13848cb721e70242e0bb198de2518
infamousbazooka <magnusfernandes1295@gmail.com>
2018-08-30 16:02:37 +0530

    Certificate ngrx functions improved

--- src/app/shared/actions/certificate.actions.ts
+++ src/app/shared/actions/certificate.actions.ts
@@ -1,6 +1,13 @@
 import { Action } from '@ngrx/store';
 import swal from 'sweetalert2';
 
+const toast = (swal as any).mixin({
+  toast: true,
+  position: 'bottom-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
 export const FETCH_ALL_CERTIFICATES_ACTION = '[Certificate] Fetch All Certificates Action';
 export const FETCH_ALL_CERTIFICATES_COMPLETE_ACTION = '[Certificate] Fetch All Certificates Complete Action';
 export const FETCH_ALL_CERTIFICATES_FAILED_ACTION = '[Certificate] Fetch All Certificates Failed Action';
@@ -36,169 +43,240 @@ export const FETCH_CERTIFICATE_FORMDATA_FAILED_ACTION = '[Certificate] Fetch Cer
 export class FetchAllCertificatesAction implements Action {
 	readonly type = FETCH_ALL_CERTIFICATES_ACTION;
 	constructor(public payload: any) {
+    toast({
+      title: 'Fetching certificates...'
+    });
+    toast.showLoading();
 	}
 }
 
 export class FetchAllCertificatesCompleteAction implements Action {
 	readonly type = FETCH_ALL_CERTIFICATES_COMPLETE_ACTION;
 	constructor(public payload: any) {
-		swal.close();
+    toast({
+      type: 'success',
+      title: 'Certificate list loaded!'
+    });
 	}
 }
 
 export class FetchAllCertificatesFailedAction implements Action {
 	readonly type = FETCH_ALL_CERTIFICATES_FAILED_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      type: 'error',
+      title: payload
+		});
+	}
 }
 
 export class FetchCertificateAction implements Action {
 	readonly type = FETCH_CERTIFICATE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      title: 'Fetching certificate...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class FetchCertificateCompleteAction implements Action {
 	readonly type = FETCH_CERTIFICATE_COMPLETE_ACTION;
-	constructor(public payload?: any) { }
+	constructor(public payload?: any) {
+    toast({
+      type: 'success',
+      title: 'Certificate loaded!'
+		});
+	}
 }
 
 export class FetchCertificateFailedAction implements Action {
 	readonly type = FETCH_CERTIFICATE_FAILED_ACTION;
-	constructor(public payload?: any) { }
+	constructor(public payload?: any) {
+    toast({
+      type: 'error',
+      title: payload
+		});
+	}
 }
 
 export class IssueCertificateAction implements Action {
 	readonly type = ISSUE_CERTIFICATE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      title: 'Issuing certificate...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class IssueCertificateCompleteAction implements Action {
 	readonly type = ISSUE_CERTIFICATE_COMPLETE_ACTION;
 	constructor(public payload?: any) {
-		swal({
-			title: "Certificate updated!",
-			type: "success",
-			timer: 3000,
-			showConfirmButton: false
-		});
+    toast({
+      type: 'success',
+      title: 'Certificate issued!'
+    });
 	}
 }
 
 export class IssueCertificateFailedAction implements Action {
 	readonly type = ISSUE_CERTIFICATE_FAILED_ACTION;
 	constructor(public payload?: any) {
-		swal("There was an error.", payload, "error");
+    toast({
+      type: 'error',
+      title: payload
+    });
 	}
 }
 
 export class CreateCertificateAction implements Action {
 	readonly type = CREATE_CERTIFICATE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      title: 'Creating certificate...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class CreateCertificateCompleteAction implements Action {
 	readonly type = CREATE_CERTIFICATE_COMPLETE_ACTION;
 	constructor(public payload: any) {
-		swal({
-			title: "Certificate Created!",
-			type: "success",
-			timer: 3000,
-			showConfirmButton: false
-		});
+    toast({
+      type: 'success',
+      title: 'Certificate created!'
+    });
 	}
 }
 
 export class CreateCertificateFailedAction implements Action {
 	readonly type = CREATE_CERTIFICATE_FAILED_ACTION;
 	constructor(public payload: any) {
-		swal("There was an error.", payload, "error");
+    toast({
+      type: 'error',
+      title: payload
+    });
 	}
 }
 
 export class UpdateCertificateAction implements Action {
 	readonly type = UPDATE_CERTIFICATE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      title: 'Updating certificate...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class UpdateCertificateCompleteAction implements Action {
 	readonly type = UPDATE_CERTIFICATE_COMPLETE_ACTION;
 	constructor(public payload?: any) {
-		swal({
-			title: "Certificate updated!",
-			type: "success",
-			timer: 3000,
-			showConfirmButton: false
-		});
+    toast({
+      type: 'success',
+      title: 'Certificate updated!'
+    });
 	}
 }
 
 export class UpdateCertificateFailedAction implements Action {
 	readonly type = UPDATE_CERTIFICATE_FAILED_ACTION;
 	constructor(public payload?: any) {
-		swal("There was an error.", payload, "error");
+    toast({
+      type: 'error',
+      title: payload
+    });
 	}
 }
 
 export class DeleteCertificateAction implements Action {
 	readonly type = DELETE_CERTIFICATE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      title: 'Deleting certificate...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class DeleteCertificateCompleteAction implements Action {
 	readonly type = DELETE_CERTIFICATE_COMPLETE_ACTION;
 	constructor(public payload?: any) {
-		swal({
-			title: "Certificate updated!",
-			type: "success",
-			timer: 3000,
-			showConfirmButton: false
-		});
+    toast({
+      type: 'success',
+      title: 'Certificate deleted!'
+    });
 	}
 }
 
 export class DeleteCertificateFailedAction implements Action {
 	readonly type = DELETE_CERTIFICATE_FAILED_ACTION;
 	constructor(public payload?: any) {
-		swal("There was an error.", payload, "error");
+    toast({
+      type: 'error',
+      title: payload
+    });
 	}
 }
 
 export class RenewCertificateAction implements Action {
 	readonly type = RENEW_CERTIFICATE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      title: 'Renewing certificate...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class RenewCertificateCompleteAction implements Action {
 	readonly type = RENEW_CERTIFICATE_COMPLETE_ACTION;
 	constructor(public payload: any) {
-		swal({
-			title: "Certificate was renewed!",
-			type: "success",
-			timer: 3000,
-			showConfirmButton: false
-		});
+    toast({
+      type: 'success',
+      title: 'Certificate renewed!'
+    });
 	}
 }
 
 export class RenewCertificateFailedAction implements Action {
 	readonly type = RENEW_CERTIFICATE_FAILED_ACTION;
 	constructor(public payload: any) {
-		swal("There was an error renewing the certificate.", payload, "error");
+    toast({
+      type: 'error',
+      title: payload
+    });
 	}
 }
 
 export class FetchCertificateFormdataAction implements Action {
 	readonly type = FETCH_CERTIFICATE_FORMDATA_ACTION;
+	constructor() {
+    toast({
+      title: 'Fetching form data...'
+    });
+		toast.showLoading();
+	}
 }
 
 export class FetchCertificateFormdataCompleteAction implements Action {
 	readonly type = FETCH_CERTIFICATE_FORMDATA_COMPLETE_ACTION;
-	constructor(public payload: any) { }
+	constructor(public payload: any) {
+    toast({
+      type: 'success',
+      title: 'Form data loaded!'
+		});
+	}
 }
 
 export class FetchCertificateFormdataFailedAction implements Action {
 	readonly type = FETCH_CERTIFICATE_FORMDATA_FAILED_ACTION;
 	constructor(public payload: any) {
-		swal("There was an error with the form.", payload, "error");
+    toast({
+      type: 'error',
+      title: payload
+    });
 	}
 }
 
--- src/app/shared/effects/certificate.effects.ts
+++ src/app/shared/effects/certificate.effects.ts
@@ -4,12 +4,16 @@ import { Effect, Actions, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, exhaustMap } from 'rxjs/operators';
 
 import * as fromCertificate from '../actions/certificate.actions';
+import { Certificate } from '../models';
 
 @Injectable()
 export class CertificateEffects {
+
+  private certificate: Certificate = new Certificate({});
+
   constructor(
     private _action$: Actions,
     private _tokenService: Angular2TokenService,
@@ -17,55 +21,86 @@ export class CertificateEffects {
   ) { }
 
   @Effect()
-  fetchCertificates$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.FETCH_ALL_CERTIFICATES_ACTION),
-    mergeMap((action: fromCertificate.FetchAllCertificatesAction) => this._tokenService.post('certificates/list', action.payload)
-      .pipe(map(response => new fromCertificate.FetchAllCertificatesCompleteAction({
-        data: response.json().message,
-        total: response.headers.get('total'),
-        per_page: response.headers.get('per-page')
-      }),
-        catchError(error => of(new fromCertificate.FetchAllCertificatesFailedAction(error.json().message)))))));
+  fetchCertificates$: Observable<Action> = this._action$.ofType(fromCertificate.FETCH_ALL_CERTIFICATES_ACTION).pipe(
+    map((action: fromCertificate.FetchAllCertificatesAction) => action.payload),
+    exhaustMap(body => this._tokenService.post('certificates/list', body)
+      .pipe(
+        map(response => new fromCertificate.FetchAllCertificatesCompleteAction({
+          data: response.json().message,
+          total: response.headers.get('total'),
+          per_page: response.headers.get('per-page')
+        })),
+        catchError(error => of(new fromCertificate.FetchAllCertificatesFailedAction(error.json().message)))
+      ))
+  );
 
   @Effect()
-  fetchCertificate$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.FETCH_CERTIFICATE_ACTION),
-    mergeMap((action: fromCertificate.FetchCertificateAction) => this._tokenService.get(`certificates/${action.payload}`)
-      .pipe(map(response => new fromCertificate.FetchCertificateCompleteAction(response.json().message),
-        catchError(error => of(new fromCertificate.FetchCertificateFailedAction(error.json().message)))))));
+  fetchCertificate$: Observable<Action> = this._action$.ofType(fromCertificate.FETCH_CERTIFICATE_ACTION).pipe(
+    map((action: fromCertificate.FetchCertificateAction) => action.payload),
+    exhaustMap(id => this._tokenService.get(`certificates/${id}`)
+      .pipe(
+        map(response => new fromCertificate.FetchCertificateCompleteAction(response.json().message)),
+        catchError(error => of(new fromCertificate.FetchCertificateFailedAction(error.json().message)))
+      ))
+  );
 
   @Effect()
-  filterCertificate$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.ISSUE_CERTIFICATE_ACTION),
-    mergeMap((action: fromCertificate.IssueCertificateAction) => this._tokenService.post(`certificates/${action.payload}/issue`, null)
-      .pipe(map(response => new fromCertificate.IssueCertificateCompleteAction(response.json().message),
-        catchError(error => of(new fromCertificate.IssueCertificateFailedAction(error.json().message)))))));
+  issueCertificate$: Observable<Action> = this._action$.ofType(fromCertificate.ISSUE_CERTIFICATE_ACTION).pipe(
+    map((action: fromCertificate.IssueCertificateAction) => action.payload),
+    exhaustMap(id => this._tokenService.post(`certificates/${id}/issue`, null)
+      .pipe(
+        map(response => new fromCertificate.IssueCertificateCompleteAction(response.json().message)),
+        catchError(error => of(new fromCertificate.IssueCertificateFailedAction(error.json().message)))
+      ))
+  );
 
   @Effect()
-  createNewCertificate$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.CREATE_CERTIFICATE_ACTION),
-    mergeMap((action: fromCertificate.CreateCertificateAction) => this._tokenService.post('certificates', action.payload)
-      .pipe(map(response => {
-        this._router.navigate(["dashboard", "certificates", "view"], { queryParams: { id: response.json().message.id } })
-        return new fromCertificate.CreateCertificateCompleteAction(response.json().message)
-      },
-        catchError(error => of(new fromCertificate.CreateCertificateFailedAction(error.json().message)))))));
+  createNewCertificate$: Observable<Action> = this._action$.ofType(fromCertificate.CREATE_CERTIFICATE_ACTION).pipe(
+    map((action: fromCertificate.CreateCertificateAction) => action.payload),
+    exhaustMap(body => this._tokenService.post('certificates', body)
+      .pipe(
+        map(response => {
+          this._router.navigate(["dashboard", "certificates", "view"], { queryParams: { id: response.json().message.id } });
+          return new fromCertificate.CreateCertificateCompleteAction(response.json().message)
+        }),
+        catchError(error => of(new fromCertificate.CreateCertificateFailedAction(error.json().message)))
+      ))
+  );
 
   @Effect()
-  deleteCertificate$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.DELETE_CERTIFICATE_ACTION),
-    mergeMap((action: fromCertificate.DeleteCertificateAction) => this._tokenService.delete(`certificates/${action.payload}`)
-      .pipe(map(response => new fromCertificate.DeleteCertificateCompleteAction(response.json().message),
-        catchError(error => of(new fromCertificate.DeleteCertificateFailedAction(error.json().message)))))));
+  deleteCertificate$: Observable<Action> = this._action$.ofType(fromCertificate.DELETE_CERTIFICATE_ACTION).pipe(
+    map((action: fromCertificate.DeleteCertificateAction) => action.payload),
+    exhaustMap(id => this._tokenService.delete(`certificates/${id}`)
+      .pipe(
+        map(() => {
+          this._router.navigate(["dashboard", "certificates"]);
+          return new fromCertificate.DeleteCertificateCompleteAction(this.certificate.id);
+        }),
+        catchError(error => of(new fromCertificate.DeleteCertificateFailedAction(error.json().message)))
+      ))
+  );
 
   @Effect()
-  updateCertificate$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.UPDATE_CERTIFICATE_ACTION),
-    mergeMap((action: fromCertificate.UpdateCertificateAction) => this._tokenService.patch(`certificates/${action.payload.id}`, action.payload)
-      .pipe(map(response => {
-        this._router.navigate(["dashboard", "certificates", "view"], { queryParams: { id: response.json().message.id } });
-        return new fromCertificate.UpdateCertificateCompleteAction(response.json().message);
-      },
-        catchError(error => of(new fromCertificate.UpdateCertificateFailedAction(error.json().message)))))));
+  updateCertificate$: Observable<Action> = this._action$.ofType(fromCertificate.UPDATE_CERTIFICATE_ACTION).pipe(
+    map((action: fromCertificate.UpdateCertificateAction) => action.payload),
+    exhaustMap(body => this._tokenService.patch(`certificates/${this.certificate.id}`, body)
+      .pipe(
+        map(response => {
+          this._router.navigate(["dashboard", "certificates", "view"], { queryParams: { id: response.json().message.id } });
+          return new fromCertificate.UpdateCertificateCompleteAction(response.json().message)
+        }),
+        catchError(error => of(new fromCertificate.UpdateCertificateFailedAction(error.json().message)))
+      ))
+  );
 
   @Effect()
-  fetchCertificateFormdata$: Observable<Action> = this._action$.pipe(ofType(fromCertificate.FETCH_CERTIFICATE_FORMDATA_ACTION),
-    mergeMap((action: fromCertificate.FetchCertificateFormdataAction) => this._tokenService.get("certificates/new")
-      .pipe(map(response => new fromCertificate.FetchCertificateFormdataCompleteAction(response.json().message),
-        catchError(error => of(new fromCertificate.FetchCertificateFormdataFailedAction(error.json().message)))))));
+  fetchCertificateFormdata$: Observable<Action> = this._action$.ofType(fromCertificate.FETCH_CERTIFICATE_FORMDATA_ACTION).pipe(
+    map((action: fromCertificate.FetchCertificateFormdataAction) => action),
+    exhaustMap(() => this._tokenService.get("certificates/new")
+      .pipe(
+        map(response => new fromCertificate.FetchCertificateFormdataCompleteAction(response.json().message)),
+        catchError(error => of(new fromCertificate.FetchCertificateFormdataFailedAction(error.json().message)))
+      ))
+  );
 
 }
