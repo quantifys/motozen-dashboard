281129b586ac817fc3f8ca026d0cc823ec64cb11
infamousbazooka <magnusfernandes1295@gmail.com>
2018-09-06 02:12:05 +0530

    Production environment url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   token_auth_config: {
-    apiBase: "https://sdrails.wafer.ee",
+    apiBase: "https://api.gemeniindia.com",
     signInRedirect: "login",
     signOutFailedValidate: true
   }
